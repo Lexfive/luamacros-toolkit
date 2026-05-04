124dc484557ebff8eb1f23542aba93b534f3a043
Ramires Mohamed <71915614+Lexfive@users.noreply.github.com>
2026-05-04 11:42:58 -0300

    Add function to bind hotkeys from profile

--- core/hotkeys.lua
+++ core/hotkeys.lua
@@ -1,1 +1,8 @@
-
+function bind_hotkeys(profile)
+    for key, action in pairs(profile) do
+        lmc_set_handler(key, function()
+            print("[HOTKEY] " .. key .. " acionado")
+            action()
+        end)
+    end
+end
