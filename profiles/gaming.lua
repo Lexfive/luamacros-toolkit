2d31294b49bdbc42c09f9d70b50215ab5028b765
Ramires Mohamed <71915614+Lexfive@users.noreply.github.com>
2026-05-04 11:41:54 -0300

    Update gaming.lua

--- profiles/gaming.lua
+++ profiles/gaming.lua
@@ -1,1 +1,19 @@
+-- Perfil: Gaming
 
+function open_browser()
+    os.execute("start chrome")
+end
+
+function mute_audio()
+    os.execute("nircmd.exe mutesysvolume 2")
+end
+
+function push_to_talk()
+    print("[GAMING] Push-to-talk ativado")
+end
+
+return {
+    F1 = open_browser,
+    F2 = mute_audio,
+    F3 = push_to_talk
+}
