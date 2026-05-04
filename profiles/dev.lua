369bc6b689700e6429bf105614c30ca0802258a3
Ramires Mohamed <71915614+Lexfive@users.noreply.github.com>
2026-05-04 11:42:06 -0300

    Add development profile with utility functions

--- profiles/dev.lua
+++ profiles/dev.lua
@@ -1,1 +1,19 @@
+-- Perfil: Dev
 
+function open_vscode()
+    os.execute("code .")
+end
+
+function open_terminal()
+    os.execute("start cmd")
+end
+
+function git_pull()
+    os.execute("git pull")
+end
+
+return {
+    F1 = open_vscode,
+    F2 = open_terminal,
+    F3 = git_pull
+}
